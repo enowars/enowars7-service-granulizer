a433abca91720dddd171d3eacf64213a50a3fb1a
ljuni26 <l.jungnickel@campus.tu-berlin.de>
2023-05-05 11:04:53 +0200

    First prototype vuln with custom linker script for building

--- src-service/main_demo_vulns.c
+++ src-service/main_demo_vulns.c
@@ -1,18 +1,32 @@
 #include <stdio.h>
 #include <string.h>
 
-//asm(".globl A");
-//asm("A = 0x42");
-char input[8] __attribute__((section(".mySection")));
-char secret[10] __attribute__((section(".mySection"))) = "FLAG{123}";
+char input[8] __attribute__((section(".d0")));
+char secret[8] __attribute__((section(".d1"))) = " FLAG{}\0";
 
+void place_flag()
+{
+    strncpy(secret, "FLAG{1}\0", 8);
+}
 
 int main()
 {
-    printf("Addr input: 0x%x\n", input);
-    printf("Addr secret: 0x%x\n", secret);
-    printf("%s", secret);
-    fgets(input, 16, stdin);
+    printf("Addr input: 0x%p\n",    (void *) input);
+    printf("Addr secret: 0x%p\n",   (void *) secret);
+    
+    char input_tmp[16];
+    fgets(input_tmp, 16, stdin);
+
+    int len = strlen(input_tmp);
+    if (len > 8)
+    {
+        printf("Too long!\n");
+    } else {
+        strncpy(input, input_tmp, 8);
+    }
+
+    place_flag();
+
     printf("You typed in:");
     printf("%s", input);
 }
