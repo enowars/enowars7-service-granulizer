6ce6af4c2c885c15ddc4ce98809a0daf8edcd8ce
ljuni26 <l.jungnickel@campus.tu-berlin.de>
2023-05-22 14:39:04 +0200

    changed granular algorithm + bug fixing

--- service/src/granular.c
+++ service/src/granular.c
@@ -81,7 +81,15 @@ void print_granular_info(const granular_info* info)
 	{
 		printf("%i,", info->order_timelens[i]);
 	}
-	printf("%i]\n", info->order_samples[info->num_samples - 1]);
+	printf("%i]\n", info->order_timelens[info->num_samples - 1]);
+
+	printf("granular_order_buffer_lens = [");
+	for (int i=0; i < info->num_samples - 1; i++)
+	{
+		printf("%i,", info->order_buffer_lens[i]);
+	}
+	printf("%i]\n", info->order_buffer_lens[info->num_samples - 1]);
+    
 }
 
 
@@ -97,12 +105,12 @@ granular_info* granulize(const char* buf, const int buf_len, char** buf_out, int
     }
 
     int grains_len = buf_len / num_grains;
-    int new_sample_len = 0;
 
 	granular_info* info = malloc(sizeof(granular_info));
 	info->num_samples = num_grains;
 	info->order_samples 	= calloc(num_grains, sizeof(int));
 	info->order_timelens 	= calloc(num_grains, sizeof(int));
+    info->order_buffer_lens = calloc(num_grains, sizeof(int));
 	info->grain_len = grains_len;
 
 	
@@ -116,17 +124,28 @@ granular_info* granulize(const char* buf, const int buf_len, char** buf_out, int
     //random length of each sample
     #define num_possible_sample_lengths 1
     int possible_sample_lengths[num_possible_sample_lengths] = {2};
+    
+    int new_sample_len = 0; //len of new sample, iterating in loop
+
     for (int i=0; i < num_grains; i++)
     {
         info->order_timelens[i] = possible_sample_lengths[rand() % num_possible_sample_lengths];
+        info->order_buffer_lens[i] = info->grain_len; //write correct buffer len for this grain
         int grains_len_here = grains_len;
         //change grains len if it is the last element
         if (i == (num_grains - 1))
         {
-            grains_len_here = buf_len - (grains_len * (num_grains - 1));
+            //info->order_timelens = new_sample_
+            info->order_buffer_lens[i] = (buf_len - new_sample_len);
+            printf("Timelen is here %i\n", info->order_timelens[i]);
+            printf("Buf Len: %i\n", buf_len);
+            printf("Sample len: %i\n", new_sample_len);
+            printf("Buffer length of last (weird) element: %i\n", info->order_buffer_lens[i]);
+            printf("Time factor of last (weird) element: %i\n", info->order_timelens[i]);
+            assert(info->order_buffer_lens[i] + (num_grains - 1) * grains_len == buf_len);
         }
         //printf("Grains len here %i for %i\n", grains_len_here, i);
-        new_sample_len += grains_len_here * info->order_timelens[i]; //calculate new sample size
+        new_sample_len += grains_len_here; //calculate new sample size
     }
     
     //granular info is created, now granulize
