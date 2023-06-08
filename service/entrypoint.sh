cfef89bae94b8e09ee7f462eba1a4cefc0853770
ljuni26 <l.jungnickel@campus.tu-berlin.de>
2023-06-08 16:37:52 +0200

    added cleanup script

--- service/entrypoint.sh
+++ service/entrypoint.sh
@@ -12,5 +12,20 @@
 
 #chown -R service:service "/users/"
 
+#!/bin/sh
+cleaner () {
+    CLEANUP_DIR=$1;
+    echo "Starting cleaner function"
+    sleep 1800 #30min
+    echo "Cleaner function will now regularly clean"
+    while true; do
+        echo "$CLEANUP_DIR" $(whoami)
+        find "$CLEANUP_DIR" -regex "$CLEANUP_DIR/.+" -mmin +30 -delete
+        sleep 60
+    done
+}
+DATA_DIR="/service/users"
+
 ncat --keep-open --listen -p 4321 --no-shutdown \
-    --wait 10s --sh-exec '/service/granulizer'
+    --wait 10s --sh-exec '/service/granulizer' &
+cleaner "$DATA_DIR"
